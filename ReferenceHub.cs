0c54be427a044f96d6d8c0cb70e3d529fe55bed3
mattymatty97 <33039934+mattymatty97@users.noreply.github.com>
2020-01-21 10:24:19 +0100

    Update ReferenceHub.cs

--- ReferenceHub.cs
+++ ReferenceHub.cs
@@ -7,48 +7,63 @@ using GameCore;
 // You can just copy and paste this. This is just a newer version of replace disconnecteds. I sure hope they use this function in Smod2 although it shouldn't matter
 public partial class ReferenceHub : MonoBehaviour
 {
-	// Token: 0x06000B4A RID: 2890 RVA: 0x0004C780 File Offset: 0x0004A980
+	// ReferenceHub
 	private void OnDestroy()
 	{
 		ReferenceHub.Hubs.Remove(base.gameObject);
 		RoleType curClass = base.gameObject.GetComponent<CharacterClassManager>().CurClass;
-		if (curClass == RoleType.Spectator || curClass == RoleType.Tutorial || curClass == RoleType.None)
+		if (curClass == RoleType.Spectator || curClass == RoleType.Tutorial)
 		{
 			return;
 		}
-		if (!ConfigFile.ServerConfig.GetBool("neon_replacedisconnecteds", true))
+		if (!ConfigFile.ModConfig.MattyMod.ReplaceDisconnects)
 		{
 			return;
 		}
-		string @string = ConfigFile.ServerConfig.GetString("neon_replace_message", "You've replaced a player that disconnected.");
+		string replaceMessage = ConfigFile.ModConfig.MattyMod.ReplaceMessage;
 		Inventory component = base.GetComponent<Inventory>();
 		Vector3 realModelPosition = base.GetComponent<PlyMovementSync>().RealModelPosition;
-		foreach (ReferenceHub referenceHub in ReferenceHub.Hubs.Values)
+		float health = base.GetComponent<PlayerStats>().health;
+		Quaternion rotation = base.transform.rotation;
+		System.Random random = new System.Random();
+		ReferenceHub[] array = (from hub in ReferenceHub.Hubs.Values
+		where hub.gameObject != PlayerManager.localPlayer
+		where hub.characterClassManager.CurClass == RoleType.Spectator
+		where !hub.serverRoles.OverwatchEnabled
+		select hub).ToArray<ReferenceHub>();
+		if (array.Length != 0)
 		{
-			if (!(PlayerManager.localPlayer == referenceHub.gameObject) && referenceHub.characterClassManager.CurClass == RoleType.Spectator)
+			ReferenceHub referenceHub = array[random.Next(array.Length)];
+			if (!(PlayerManager.localPlayer == referenceHub.gameObject) && referenceHub.characterClassManager.CurClass == RoleType.Spectator && !referenceHub.gameObject.GetComponent<CharacterClassManager>().GetComponent<ServerRoles>().OverwatchEnabled)
 			{
 				referenceHub.characterClassManager.SetPlayersClass(curClass, referenceHub.gameObject, true, false);
 				if (curClass == RoleType.Scp079)
 				{
 					this.Copy079(referenceHub.scp079PlayerScript, base.GetComponent<Scp079PlayerScript>());
 				}
-				Timing.RunCoroutine(this.ReallyFunCoroutine(realModelPosition, referenceHub.gameObject), 1);
+				Timing.RunCoroutine(this.ReallyFunCoroutine(realModelPosition, rotation, health, referenceHub.gameObject), 1);
 				foreach (Inventory.SyncItemInfo syncItemInfo in component.items)
 				{
 					referenceHub.inventory.AddNewItem(syncItemInfo.id, syncItemInfo.durability, syncItemInfo.modSight, syncItemInfo.modBarrel, syncItemInfo.modOther);
 				}
-				PlayerManager.localPlayer.GetComponent<Broadcast>().TargetAddElement(referenceHub.characterClassManager.connectionToClient, @string, 7U, false);
-				break;
+				PlayerManager.localPlayer.GetComponent<Broadcast>().TargetAddElement(referenceHub.characterClassManager.connectionToClient, replaceMessage, 7U, false);
 			}
 		}
 	}
+
 	
-	private IEnumerator<float> ReallyFunCoroutine(Vector3 pos, GameObject go)
+	private IEnumerator<float> ReallyFunCoroutine(Vector3 pos, Quaternion rot, float health, GameObject go)
 	{
 		yield return Timing.WaitForSeconds(0.3f);
 		go.GetComponent<PlyMovementSync>().OverridePosition(pos, 0f, false);
+		yield return Timing.WaitForSeconds(0.5f);
+		go.transform.rotation = rot;
+		PlayerStats component = go.GetComponent<PlayerStats>();
+		component.health = health;
+		component.TargetSyncHp(component.connectionToClient, health);
 		yield break;
 	}
+
 	private void Copy079(Scp079PlayerScript dst, Scp079PlayerScript src) {
 		dst.currentCamera = src.currentCamera;
 		dst.currentRoom = src.currentRoom;
