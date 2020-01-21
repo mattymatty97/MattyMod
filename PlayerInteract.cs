f74e7497f2cb54848d5ea2a4c3d7d2cc11b5f76a
mattymatty97 <33039934+mattymatty97@users.noreply.github.com>
2020-01-21 10:10:36 +0100

    added userConsole door change broadcast
    for punishing door trollers

--- PlayerInteract.cs
+++ PlayerInteract.cs
@@ -1,4 +1,5 @@
-	// This is just RemoteKeycard.
+	// before every door changeState make a call to broadcastDoor(GameObject door)
+	// This is RemoteKeycard.
 	public void CallCmdOpenDoor(GameObject doorId)
 	{
 		//before SCP check
@@ -30,6 +31,37 @@
 			catch
 			{
 				this.RpcDenied(doorId);
+			}
+		}
+	}
+
+	private void broadcastDoor(GameObject door)
+	{
+		IEnumerable<GameObject> players = PlayerManager.players;
+		int status = door.GetComponent<Door>().status;
+		if (status == 0 || status == 1)
+		{
+			IEnumerable<GameObject> source = players;
+			Func<GameObject, bool> <>9__0;
+			Func<GameObject, bool> predicate;
+			if ((predicate = <>9__0) == null)
+			{
+				predicate = (<>9__0 = ((GameObject p) => Vector3.Distance(p.transform.position, door.gameObject.transform.position) < PlayerInteract.distance));
+			}
+			foreach (GameObject gameObject in source.Where(predicate))
+			{
+				CharacterClassManager component = gameObject.GetComponent<CharacterClassManager>();
+				component.TargetConsolePrint(component.connectionToClient, string.Concat(new object[]
+				{
+					"Player: <color=green>",
+					base.gameObject.GetComponent<NicknameSync>().MyNick,
+					"</color> ",
+					(status == 0) ? "Openned" : "Closed",
+					" ",
+					door.name,
+					" :",
+					door.GetComponent<NetworkIdentity>().assetId
+				}), "grey");
 			}
 		}
 	}
