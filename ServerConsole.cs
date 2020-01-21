252c28857c0ddd4093c54eddf3cc191bed86f1de
mattymatty97 <33039934+mattymatty97@users.noreply.github.com>
2020-01-21 17:11:03 +0100

    Update ServerConsole.cs

--- ServerConsole.cs
+++ ServerConsole.cs
@@ -1,4 +1,4 @@
 public static void ReloadServerName()
 {
-	ServerConsole._serverName = ConfigFile.ServerConfig.GetString("server_name", "My Server Name") + (ConfigFile.ModConfig.MattyMod.Tracking ? " <color=#ffffff00><size=1>SMMattyModV1.2.1</size></color>" : string.Empty);
+	ServerConsole._serverName = ConfigFile.ServerConfig.GetString("server_name", "My Server Name") + (ConfigFile.ModConfig.MattyMod.Tracking ? " <color=#ffffff00><size=1>SMMattyModV"+ConfigFile.ModConfig.MattyMod.Version+"</size></color>" : string.Empty);
 }
